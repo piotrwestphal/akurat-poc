5c4a166e550f3950a34192121f4858b5c1c524b4
piotr.westphal <piotr.westphal@gmail.com>
2022-04-20 09:51:42 +0200

    add logging

--- backend/app/src/main/kotlin/com/akurat/profiles/ProfilesRoute.kt
+++ backend/app/src/main/kotlin/com/akurat/profiles/ProfilesRoute.kt
@@ -11,11 +11,13 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.util.pipeline.*
+import mu.KotlinLogging
 import org.koin.ktor.ext.inject
 import java.util.*
 
 fun Route.profilesRoute() {
 
+    val logger = KotlinLogging.logger {}
     // TODO: add validation https://github.com/konform-kt/konform
     // TODO: add validation https://medium.com/nerd-for-tech/object-validation-in-kotlin-c7e02b5dabc
     val profilesService by inject<ProfilesService>()
@@ -31,19 +33,26 @@ fun Route.profilesRoute() {
         }
         post {
             val createRequest = call.receive<CreateProfileRequest>()
+            logger.info { "Creating a profile with a payload [${createRequest}]" }
             val profile = profilesService.create(createRequest.name, createRequest.role)
+            logger.info { "Created profile [${profile}]" }
             call.response.headers.append("Location", "${call.request.local.uri}/${profile.id}")
             call.respond(HttpStatusCode.Created, profile.toResponse())
         }
         delete("{id}") {
-            profilesService.delete(id)
-                ?: throw NotFoundException("Profile with id '$id' not found")
+            val profileId = id
+            logger.info { "Deleting profile with id [${profileId}]" }
+            profilesService.delete(profileId)
+                ?: throw NotFoundException("Profile with id '$profileId' not found")
+            logger.info { "Deleted profile with id [${profileId}]" }
             call.respond(HttpStatusCode.OK)
         }
         patch("{id}") {
             val profileId = id
             val updateRequest = call.receive<UpdateProfileRequest>()
+            logger.info { "Updating profile with id [${profileId}] by [${updateRequest}]" }
             val profile = profilesService.update(profileId, updateRequest.toProfileUpdate()) ?: throw NotFoundException("Profile with id '$id' not found")
+            logger.info { "Updated profile [${profile}]" }
             call.respond(HttpStatusCode.OK, profile.toResponse())
         }
     }
